3c3267ec208f5253cfc8a296d596639f336c8e94
Kamil Cinner <kamil.cinner@gmail.com>
2021-04-13 13:37:57 +0200

    Initial commit

---
+++ app/src/main/java/pollub/ism/lab06/MainActivity.java
@@ -0,0 +1,53 @@
+package pollub.ism.lab06;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+
+import pollub.ism.lab06.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private ArrayAdapter<CharSequence> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        adapter = ArrayAdapter.createFromResource(this, R.array.Asortyment, android.R.layout.simple_dropdown_item_1line);
+        binding.spinner.setAdapter(adapter);
+
+        binding.przyciskSkladuj.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Tutaj kod do wykonania
+            }
+        });
+
+        binding.przyciskWydaj.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Tutaj kod do wykonia
+            }
+        });
+
+        binding.spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                //Tutaj kod do wykoania
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                //Nie będziemy implementować, ale musi być
+            }
+        });
+    }
+}
